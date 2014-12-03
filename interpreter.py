fde82efd3aeae645b2dfebdd7dfd0cce88bcd3bd
Gavin Mendel-Gleason <jacobian@gmail.com>
2014-12-03 20:28:08 +0000

    First commit

--- interpreter.py
+++ interpreter.py
@@ -13,6 +13,8 @@ from effects.EffectsParser import EffectsParser
 from predicate.PredicateLexer import PredicateLexer
 from predicate.PredicateParser import PrediateParser
 
+import MySQLdb 
+from config import * 
 
 
 """ 
@@ -29,20 +31,6 @@ args := term | term "," term
 term := var | literal | unary | random 
 predicate := "ident(" args ")"
 
-"""
-
-def parsePredicate(string): 
-    char_stream = ANTLRStringStream(string.decode('utf-8'))
-    lexer = PredicateLexer(char_stream)
-    tokens = CommonTokenStream(lexer)
-    parser = PredicateParser(tokens);
-
-    try:
-        print parser.statement()
-    except RecognitionException:
-        traceback.print_stack()
-
-"""
 The Grammar of effects is as follows: 
 
 var := [char]
@@ -59,6 +47,18 @@ assign := var " := " statement
 clause := assign "\n" clause
 """
 
+
+def parsePredicate(string): 
+    char_stream = ANTLRStringStream(string.decode('utf-8'))
+    lexer = PredicateLexer(char_stream)
+    tokens = CommonTokenStream(lexer)
+    parser = PredicateParser(tokens);
+
+    try:
+        print parser.statement()
+    except RecognitionException:
+        traceback.print_stack()
+
 def parseEffects(string): 
     char_stream = ANTLRStringStream(sys.argv[1].decode('utf-8'))
     lexer = EffectsLexer(char_stream)
@@ -70,3 +70,32 @@ def parseEffects(string):
     except RecognitionException:
         traceback.print_stack()
 
+LIBRARY = []
+
+
+
+def lookup(atom, library): 
+    for pred in library: 
+        com = pred[0]
+        if com == atom: 
+            return (pred[1],pred[2])
+    return None
+
+def evalPredicate(exp,env={}): 
+    name = exp[0]
+    if name == 'cong': 
+        fst = exp[1]
+        snd = exp[2]
+    elif name == 'disj': 
+        fst = exp[1] 
+        snd = exp[2]
+    elif name == 'neg':
+        pass
+    elif name == 'predicate': 
+        cmd = exp[1]
+        args = exp[2:]
+        result = lookup(exp[1])
+        if result:
+            (params,body) = result 
+            newenv = unify(args,env)
+            substitue(args,params,body,env)
